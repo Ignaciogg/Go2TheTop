b6118052c31f4bc09bac02afb3eb1fee549ce5e8
Ignaciogg <nachogilgarzon@gmail.com>
2021-11-04 16:28:36 +0100

    loginview y controlador skeleton

--- src/control/controlLogin.java
+++ src/control/controlLogin.java
@@ -1,11 +1,25 @@
 package control;
 
-import com.jfoenix.controls.JFXTextArea;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class controlLogin {
 
     @FXML
-    private JFXTextArea miTextoField;
+    private TextField userNameText;
+
+    @FXML
+    private PasswordField passwordText;
+
+    @FXML
+    private Button botonLogin;
+
+    @FXML
+    void comprobarLogin(ActionEvent event) {
+
+    }
 
 }
