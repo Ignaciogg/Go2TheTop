b841ef2eb34819a19c68355c8808497585587d2e
Ignaciogg <nachogilgarzon@gmail.com>
2021-11-04 16:38:21 +0100

    login main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,35 +1,34 @@
 package application;
 
+import java.io.IOException;
 import control.controlLogin;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	public String usuarioLogin;
 
-	@Override
+     @Override
+        public void start(Stage primaryStage) throws Exception {
+            try {
+            	FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/viewLogin.fxml"));
+    			controlLogin control = new controlLogin();
+    			loader.setController(control);
+    			Parent root = loader.load();
+    			primaryStage.setTitle("gO2theTop - Login");
+    			primaryStage.setScene(new Scene(root));
+    			primaryStage.show();
 
-	public void start(Stage primaryStage) {
-	try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/viewLogin.fxml"));
-			controlLogin control = new controlLogin();
-			loader.setController(control);
-			Parent root = loader.load();
-			primaryStage.setTitle("gO2theTop - Login");
-			primaryStage.setScene(new Scene(root));
-			primaryStage.show();
+                }catch (IOException f) {
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+                    f.printStackTrace();
+                }
+        }
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
