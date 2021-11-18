a6c1d88c8b348a1a227eae8648bbc62af538ebaa
Mario Uceda <marioucedayeves@gmail.com>
2021-11-18 15:53:00 +0100

    files

--- src/model/Administrador.java
+++ src/model/Administrador.java
@@ -18,29 +18,5 @@ public class Administrador extends Usuario{
 		
 	}
 	
-	public void escribirLogin(Usuario user){
-        Gson gson = new Gson();
-        try{
-            BufferedWriter bw = new BufferedWriter(new FileWriter("src/files/login.jsonl",true));
-            bw.newLine();
-            bw.append(gson.toJson(user));
-            bw.flush();
-            bw.close();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-    }
-
-    public void escribirPersona(Usuario nuevo, String ruta){
-        Gson gson = new Gson();
-        try{
-            BufferedWriter bw = new BufferedWriter(new FileWriter(ruta));
-            bw.write(gson.toJson(nuevo));
-            bw.flush();
-            bw.close();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-    }
 	
 }
