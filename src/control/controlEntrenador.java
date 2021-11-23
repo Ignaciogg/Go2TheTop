2bca6970fa47b6b2ca7bb8e6f010a502828444e9
sfms97 <feernandomemdez@gmail.com>
2021-11-23 16:36:22 +0100

    Enlace deportista entrenador 1

--- src/control/controlEntrenador.java
+++ src/control/controlEntrenador.java
@@ -1,27 +1,66 @@
 package control;
 
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.print.PrintColor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import model.Administrador;
+import model.Deportista;
 import model.Usuario;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Properties;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import com.jfoenix.controls.JFXTextField;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import application.ficheros;
 
 public class controlEntrenador {
 
 	private Usuario user;
 
 	@FXML
-    private Text nombreUser;
+    private JFXTextField nombreUser;
 
     @FXML
     private Button botonCerrarSesion;
+    
+    @FXML
+    private TableView<Deportista> tablaEntrenador;
 
+    @FXML
+    private TableColumn<Deportista, String> colDeportista;
 
+    @FXML
+    private TableColumn<Deportista, String> colEmail;
+
+    @FXML
+    private TableColumn<Deportista, String> colGenero;
+
+    @FXML
+    private TableColumn<Deportista, String> colCumple;
+    
+    ArrayList<Deportista> deportistaArray;
+    
+    
+
+    
     @FXML
     void cerrarSesion(ActionEvent event) {
 		 try {
@@ -44,11 +83,92 @@ public class controlEntrenador {
 			}
 
 	    }
+    
+    private void inicializarTabla(Usuario user) {
+    	/*tablaEntrenador = new TableView();
+    	colDeportista = new TableColumn<>("Deportista");
+    	colEmail = new TableColumn<>("Email");
+    	colGenero = new TableColumn<>("Genero");
+    	colCumple = new TableColumn<>("Cumpleanos");
+    	tablaEntrenador.getColumns().addAll(colDeportista,colEmail,colGenero,colCumple);*/
+    	
+		colDeportista.setCellValueFactory(new PropertyValueFactory<Deportista,String>("name"));
+		colEmail.setCellValueFactory(new PropertyValueFactory<Deportista,String>("email"));
+		colGenero.setCellValueFactory(new PropertyValueFactory<Deportista,String>("genre"));
+		colCumple.setCellValueFactory(new PropertyValueFactory<Deportista,String>("birthdate"));
+		
+		tablaEntrenador.setItems(observableList(user));
+		//deportistas = FXCollections.observableArrayList();
+		
+		/*Deportista dep1 = new Deportista("1", "yahoo.com", "pass123","deportista", "alfonso", "Tupu", "26-04-98", "no_binario", true);
+		Deportista dep2 =  new Deportista("2", "yahoo2.com", "pass123","deportista", "alfonso2", "Tupu2", "26-04-98", "no_binario", true);*/
+		
+		//tablaEntrenador.getItems().addAll(dep1,dep2);
+		
+		
+	}
+    
+    private String obtenerDatosFicheroEnlaces(Usuario user) {
+    	Gson gson = new Gson();
+    	
+    	String fichero = "", entrenID, deporID="";
+    	 
+    	try (BufferedReader br = new BufferedReader(new FileReader("src/files/enlaces.jsonl"))) {
+    	    String linea;
+    	    while ((linea = br.readLine()) != null) {
+    	        fichero += linea;
+    	        System.out.print(fichero);
+    	        Properties properties = gson.fromJson(fichero, Properties.class);
+    	        
+        	    entrenID=(String) properties.get("entrenadorID");
+        	    System.out.print(entrenID);
+            	System.out.print(user.getUserId());
+        	    
+            	if(user.getUserId().equals(entrenID)) {
+            		deporID= (String) properties.get("deportistaID");
+            		System.out.print(deporID);
+            	}
+    	    }
+        	
+        	System.out.print(deporID);
+    	 
+    	} catch (FileNotFoundException ex) {
+    	    System.out.println(ex.getMessage());
+    	} catch (IOException ex) {
+    	    System.out.println(ex.getMessage());
+    	}
+    	
+    	return deporID;
+
+    }
+    
+    public ObservableList<Deportista> observableList(Usuario user){
+        ObservableList<Deportista> deportistas = FXCollections.observableArrayList();
+                
+        deportistaArray.add(new ficheros().leerDeportista("src/files/deportistas/" + obtenerDatosFicheroEnlaces(user) +".jsonl"));
+        
+        /*Deportista dep1 = new Deportista("1", "yahoo.com", "pass123","deportista", "alfonso", "Tupu", "26-04-98", "no_binario", true);
+        deportistaArray.add(dep1);*/
+        
+        deportistas.addAll(deportistaArray);
+        return deportistas;
+    }
+	
 
 	public void setUsuario(Usuario u) {
     	user = u;
-    	nombreUser.setText(u.getName());
+    	if(u.getGenre().equals("hombre")){
+    		nombreUser.setText("Bienvenido " + u.getName());
+    	}else if(u.getGenre().equals("mujer")) {
+    		nombreUser.setText("Bienvenida " + u.getName());
+    	}
+    	
+    	this.inicializarTabla(u);
+    	
 
     }
+	
+	
+	
 
 }
