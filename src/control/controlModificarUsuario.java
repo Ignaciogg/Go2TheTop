a48a84276dcf3b6db59ce25d3c008220791118f0
Alejandro Fernández Gete <22171935@live.uem.es>
2021-11-24 15:47:21 +0100

    Control Modificar usuario

---
+++ src/control/controlModificarUsuario.java
@@ -0,0 +1,14 @@
+package control;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+
+
+public class controlModificarUsuario {
+	
+	private Button botonVolver;
+
+}
