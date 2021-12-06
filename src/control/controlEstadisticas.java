9d20911f94703e64ec895c0658844ceba012ee50
Ignaciogg <nachogilgarzon@gmail.com>
2021-12-06 23:58:14 +0100

    mas grafica

--- src/control/controlEstadisticas.java
+++ src/control/controlEstadisticas.java
@@ -1,9 +1,15 @@
 package control;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
@@ -14,7 +20,7 @@ import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 import model.Deportista;
 
-public class controlEstadisticas {
+public class controlEstadisticas implements Initializable{
 
 	private Deportista user;
 
@@ -22,49 +28,54 @@ public class controlEstadisticas {
     private Button botonVolver;
 
     @FXML
-    private RadioButton frecuencia;
-
-    @FXML
     private ToggleGroup grafica2;
 
     @FXML
-    private RadioButton actividad;
+    private RadioButton frecuencia;
 
     @FXML
-    private RadioButton temperatura;
+    private RadioButton nivelo2;
 
     @FXML
     private LineChart<?, ?> grafica;
 
     @FXML
+    private DatePicker datepicker1;
+
+    @FXML
+    private DatePicker datepicker2;
+
+    @FXML
     void mostrarFrecuencia(ActionEvent event) {
     	System.out.println("Mostrar Frecuencia Cardiaca");
-
     	grafica.getData().clear();
-		Series<String, Number> series = new XYChart.Series<String, Number>();
 
-		for(int i = 2000; i<=2400; i+=100){
-			series.getData().add(new XYChart.Data<String, Number>("pepe", i));
-		}
+    	XYChart.Series serie1 = new XYChart.Series();
+		serie1.getData().add(new XYChart.Data("1", 85));
+		serie1.getData().add(new XYChart.Data("2", 100));
+		serie1.getData().add(new XYChart.Data("3", 110));
+		serie1.getData().add(new XYChart.Data("4", 120));
+		serie1.getData().add(new XYChart.Data("5", 120));
+		serie1.getData().add(new XYChart.Data("6", 115));
 
-		series.setName("Actividad");
-		//grafica.getData().add(series);
+		grafica.getData().add(serie1);
 
     }
 
     @FXML
     void mostrarNivelO2(ActionEvent event) {
     	System.out.println("Mostrar Nivel O2");
-
     	grafica.getData().clear();
-		Series<String, Number> series = new XYChart.Series<String, Number>();
 
-		for(int i = 2000; i<=2400; i+=100){
-			series.getData().add(new XYChart.Data<String, Number>("pepe", i));
-		}
+    	XYChart.Series serie1 = new XYChart.Series();
+		serie1.getData().add(new XYChart.Data("1", 99));
+		serie1.getData().add(new XYChart.Data("2", 97));
+		serie1.getData().add(new XYChart.Data("3", 94));
+		serie1.getData().add(new XYChart.Data("4", 91));
+		serie1.getData().add(new XYChart.Data("5", 89));
+		serie1.getData().add(new XYChart.Data("6", 85));
 
-		series.setName("Actividad");
-		//grafica.getData().add(series);
+		grafica.getData().add(serie1);
 
     }
 
@@ -94,5 +105,15 @@ public class controlEstadisticas {
 	public void setUsuario(Deportista u) {
     	user = u;
     }
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		XYChart.Series serie = new XYChart.Series();
+		serie.getData().add(new XYChart.Data("1", 85));
+		serie.getData().add(new XYChart.Data("2", 100));
+		serie.getData().add(new XYChart.Data("3", 110));
+
+		grafica.getData().add(serie);
+	}
 
 }
