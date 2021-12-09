468bc5a8fc0e2b0051583491e01bae00a1bf98d7
Alejandro Fernández Gete <22171935@live.uem.es>
2021-12-09 16:35:00 +0100

    Chat

---
+++ src/control/controlChat.java
@@ -0,0 +1,42 @@
+package control;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import model.Deportista;
+
+public class controlChat {
+	
+	private Deportista user;
+
+    @FXML
+    private Button botonVolver;
+
+    @FXML
+    void volver(ActionEvent event) {
+    	try {
+
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/viewDepor.fxml"));
+			controlDeportista controlDepor = new controlDeportista();
+			loader.setController(controlDepor);
+			Parent root = loader.load();
+			controlDepor.setUsuario(user);
+
+			Stage stage = (Stage) botonVolver.getScene().getWindow();
+			stage.setTitle("gO2theTop - Deportista");
+
+			stage.setScene(new Scene(root));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+
+    public void setUsuario(Deportista u) {
+    	user = u;
+    }
+}
